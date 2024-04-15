088eb421333a79cbfc55ecce6e952b398685ffeb
Lbongard <reid.bongard@gmail.com>
2024-04-15 12:30:01 -0700

    Removed extraneous Terraform files and added variable to Main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,14 +2,20 @@
 # with standard storage
 
 provider "google" {
-  project = <project_id>
-  credentials = <path to credentials>
+  project = "<your project id>"
+  credentials = "<path to your credentials>"
   region  = "US"
+}
+
+variable "gcs_bucket_name" {
+  description = "My Storage Bucket Name"
+  #Update the below to a unique bucket name
+  # default     = "congress_data"
 }
 
 # Define top-level bucket
 resource "google_storage_bucket" "static" {
- name          = "congress_data"
+ name          = var.gcs_bucket_name
  location      = "US"
  storage_class = "STANDARD"
  public_access_prevention = "enforced"
@@ -17,31 +23,31 @@ resource "google_storage_bucket" "static" {
  uniform_bucket_level_access = true
 }
 
-# Define subdirectories (placeholders) by creating empty objects
-resource "google_storage_bucket_object" "bills_subdir" {
-  bucket = google_storage_bucket.static.name
-  name   = "bills/"
-  source = "/dev/null"  # Use /dev/null as the source to create an empty object
-}
+# # Define subdirectories (placeholders) by creating empty objects
+# resource "google_storage_bucket_object" "bills_subdir" {
+#   bucket = google_storage_bucket.static.name
+#   name   = "bills/"
+#   source = "/dev/null"  # Use /dev/null as the source to create an empty object
+# }
 
-resource "google_storage_bucket_object" "actions_subdir" {
-  bucket = google_storage_bucket.static.name
-  name   = "actions/"
-  source = "/dev/null"  # Use /dev/null as the source to create an empty object
-}
+# resource "google_storage_bucket_object" "actions_subdir" {
+#   bucket = google_storage_bucket.static.name
+#   name   = "actions/"
+#   source = "/dev/null"  # Use /dev/null as the source to create an empty object
+# }
 
-resource "google_storage_bucket_object" "member_subdir" {
-  bucket = google_storage_bucket.static.name
-  name   = "member/"
-  source = "/dev/null"  # Use /dev/null as the source to create an empty object
-}
+# resource "google_storage_bucket_object" "member_subdir" {
+#   bucket = google_storage_bucket.static.name
+#   name   = "member/"
+#   source = "/dev/null"  # Use /dev/null as the source to create an empty object
+# }
 
-resource "google_storage_bucket_object" "votes_subdir" {
-  bucket = google_storage_bucket.static.name
-  name   = "votes/"
-  source = "/dev/null"  # Use /dev/null as the source to create an empty object
-}
+# resource "google_storage_bucket_object" "votes_subdir" {
+#   bucket = google_storage_bucket.static.name
+#   name   = "votes/"
+#   source = "/dev/null"  # Use /dev/null as the source to create an empty object
+# }
 
-# Upload a text file as an object
-# to the storage bucket
+# # Upload a text file as an object
+# # to the storage bucket
 

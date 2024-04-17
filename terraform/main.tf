16295cda559379c8672e47a43d2edf3b08fe0996
Lbongard <reid.bongard@gmail.com>
2024-04-16 20:41:43 -0700

    Removed extra code from main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,31 +35,4 @@ resource "google_bigquery_dataset" "dataset" {
   location   = "US"
 }
 
-# # Define subdirectories (placeholders) by creating empty objects
-# resource "google_storage_bucket_object" "bills_subdir" {
-#   bucket = google_storage_bucket.static.name
-#   name   = "bills/"
-#   source = "/dev/null"  # Use /dev/null as the source to create an empty object
-# }
-
-# resource "google_storage_bucket_object" "actions_subdir" {
-#   bucket = google_storage_bucket.static.name
-#   name   = "actions/"
-#   source = "/dev/null"  # Use /dev/null as the source to create an empty object
-# }
-
-# resource "google_storage_bucket_object" "member_subdir" {
-#   bucket = google_storage_bucket.static.name
-#   name   = "member/"
-#   source = "/dev/null"  # Use /dev/null as the source to create an empty object
-# }
-
-# resource "google_storage_bucket_object" "votes_subdir" {
-#   bucket = google_storage_bucket.static.name
-#   name   = "votes/"
-#   source = "/dev/null"  # Use /dev/null as the source to create an empty object
-# }
-
-# # Upload a text file as an object
-# # to the storage bucket
 
